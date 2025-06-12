817ee0eb31b88a27e635551c904e94ad7d286147
ArgHero <saul.hernandez.ipn@gmail.com>
2025-06-11 20:49:14 -0600

    Cambios visuales del pryecto

--- src/main.js
+++ src/main.js
@@ -4,6 +4,29 @@ const numeroSecreto = Math.floor(Math.random() * 100) + 1;
 const inputNumero = document.getElementById('numero');
 const botonAdivinar = document.getElementById('adivinar');
 const mensaje = document.getElementById('mensaje');
+const btnMenos = document.getElementById("btnMenos");
+const btnMas = document.getElementById("btnMas");
+
+btnMenos.addEventListener("click",()=>{
+    const valor=parseInt(inputNumero.value)
+    inputNumero.value = valor-1;
+})
+
+btnMas.addEventListener("click",()=>{
+    const valor=parseInt(inputNumero.value)
+    inputNumero.value = valor+1;
+})
+
+inputNumero.addEventListener("change",()=>{
+    const valor=parseInt(inputNumero.value)
+    if(valor >100)
+        inputNumero.value="100"
+    else if (valor < 0)
+        inputNumero.value="0"
+    else
+        inputNumero.value="0"
+
+})
 
 botonAdivinar.addEventListener('click', () => {
     const numeroJugador = parseInt(inputNumero.value);
